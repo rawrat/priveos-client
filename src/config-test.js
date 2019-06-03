46904cfec5c4e55bca0cc01db0a79529b0d5be77
fabifrank <info@fabifrank.com>
2019-06-03 09:08:41 +0200

    remove indent

--- src/config-test.js
+++ src/config-test.js
@@ -2,6 +2,6 @@ export default {
   dappContract: 'dappcontract',
   priveosContract: 'priveosrules',
   httpEndpoint: 'http://localhost:8888',
-	chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
+  chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
   brokerUrl: 'http://localhost:4000',
 }
