6a9467aa081381db85408bf3b220d74783a189c0
fabifrank <info@fabifrank.com>
2019-06-03 09:08:13 +0200

    cleanup

--- src/config-test.js
+++ src/config-test.js
@@ -2,6 +2,6 @@ export default {
   dappContract: 'dappcontract',
   priveosContract: 'priveosrules',
   httpEndpoint: 'http://localhost:8888',
-  chainId: '3a953df32658a2c7ef97c4048acb255969e803dafd92b96755bbc90ce4d1e448',
-  brokerUrl: 'http://localhost:8001',
+	chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
+  brokerUrl: 'http://localhost:4000',
 }
